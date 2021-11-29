65c29a7bbd33bbca6229cab68dbc40d4eb6b3edb
Vitor Pamplona <vitor@vitorpamplona.com>
2021-11-29 17:53:06 -0500

    Adding CQL evaluation to the Test Suite

---
+++ app/src/test/java/org/who/ddccverifier/CQLTest.kt
@@ -0,0 +1,99 @@
+package org.who.ddccverifier
+
+import ca.uhn.fhir.context.FhirContext
+import org.hl7.fhir.r4.model.Bundle
+import org.junit.Test
+import org.opencds.cqf.cql.engine.execution.CqlLibraryReader
+import org.cqframework.cql.cql2elm.CqlTranslator
+import java.io.File
+import org.cqframework.cql.cql2elm.LibraryManager
+import org.cqframework.cql.cql2elm.ModelManager
+import org.fhir.ucum.UcumEssenceService
+
+import java.io.StringReader
+import java.lang.IllegalArgumentException
+import java.net.URLDecoder
+import org.cqframework.cql.cql2elm.FhirLibrarySourceProvider
+import org.opencds.cqf.cql.engine.execution.Context
+
+import ca.uhn.fhir.context.FhirVersionEnum
+import org.junit.Assert.*
+import org.opencds.cqf.cql.engine.fhir.model.R4FhirModelResolver
+import org.opencds.cqf.cql.evaluator.engine.retrieve.BundleRetrieveProvider
+import org.opencds.cqf.cql.evaluator.engine.terminology.BundleTerminologyProvider
+import org.opencds.cqf.cql.engine.data.CompositeDataProvider
+import org.hl7.elm.r1.VersionedIdentifier
+import org.opencds.cqf.cql.engine.execution.InMemoryLibraryLoader
+import org.opencds.cqf.cql.evaluator.fhir.DirectoryBundler
+
+
+class CQLTest {
+
+    private fun open(assetName: String): String {
+        return javaClass.classLoader?.getResourceAsStream(assetName)?.bufferedReader()
+            .use { bufferReader -> bufferReader?.readText() } ?: ""
+    }
+
+    private val modelManager = ModelManager()
+    private val libraryManager = LibraryManager(modelManager)
+    private val ucumService =
+        UcumEssenceService(UcumEssenceService::class.java.getResourceAsStream("/ucum-essence.xml"))
+
+    /**
+     * Translate CQL to XML and loads the XML as a Library
+     */
+    private fun loadRules(file: String): org.cqframework.cql.elm.execution.Library? {
+        libraryManager.librarySourceLoader.registerProvider(FhirLibrarySourceProvider())
+        val cqlFile = File(URLDecoder.decode(javaClass.classLoader?.getResource(file)?.file, "UTF-8"))
+        val translator = CqlTranslator.fromFile(cqlFile, modelManager, libraryManager, ucumService)
+        if (translator.errors.size > 0) {
+            System.err.println("Translation failed due to errors:")
+            val errors: ArrayList<String> = ArrayList()
+            for (error in translator.errors) {
+                val tb = error.locator
+                val lines = if (tb == null) "[n/a]" else String.format("[%d:%d, %d:%d]",
+                    tb.startLine, tb.startChar, tb.endLine, tb.endChar)
+                System.err.printf("%s %s%n", lines, error.message)
+                errors.add(lines + error.message)
+            }
+            throw IllegalArgumentException(errors.toString())
+        }
+        assertEquals(0, translator.errors.size)
+        val xml = translator.toXml()
+        return CqlLibraryReader.read(StringReader(xml))
+    }
+
+    @Test
+    fun evaluatePatient() {
+        val assetBundle = FhirContext.forR4().newJsonParser().parseResource(open("LibraryTestPatient.json")) as Bundle
+        assertEquals("48d1906f-82df-44d2-9d26-284045504ba9", assetBundle.id)
+
+        val cqlLibrary = loadRules("LibraryTestRules.cql")
+        val context = Context(cqlLibrary)
+
+        val fhirContext = FhirContext.forCached(FhirVersionEnum.R4)
+        val bundleRetrieveProvider = BundleRetrieveProvider(fhirContext, assetBundle)
+
+        val fhirHelperSource = libraryManager.librarySourceLoader.getLibrarySource(VersionedIdentifier().withId("FHIRHelpers").withVersion("4.0.0"))
+        val translator = CqlTranslator.fromStream(fhirHelperSource, modelManager, libraryManager, ucumService)
+        val fhirHelpers = CqlLibraryReader.read(StringReader(translator.toXml()))
+
+        val libraryLoader = InMemoryLibraryLoader(arrayListOf(fhirHelpers))
+        context.registerLibraryLoader(libraryLoader)
+
+        val r4ModelResolver = R4FhirModelResolver()
+        val r4Provider = CompositeDataProvider(r4ModelResolver, bundleRetrieveProvider)
+
+        context.registerDataProvider("http://hl7.org/fhir", r4Provider)
+
+        assertEquals(true, context.resolveExpressionRef("AgeRange-548").evaluate(context))
+        assertEquals(true, context.resolveExpressionRef("Essential hypertension (disorder)").evaluate(context))
+        assertEquals(false, context.resolveExpressionRef("Malignant hypertensive chronic kidney disease (disorder)").evaluate(context))
+        assertEquals(true, context.resolveExpressionRef("MeetsInclusionCriteria").evaluate(context))
+        assertEquals(false, context.resolveExpressionRef("MeetsExclusionCriteria").evaluate(context))
+        assertEquals(true, context.resolveExpressionRef("InPopulation").evaluate(context))
+        assertEquals("", context.resolveExpressionRef("Recommendation").evaluate(context))
+        assertNull(context.resolveExpressionRef("Rationale").evaluate(context))
+        assertNull(context.resolveExpressionRef("Errors").evaluate(context))
+    }
+}
