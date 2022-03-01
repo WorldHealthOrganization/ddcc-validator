513da8248f3ce08533e0b40e421fe13c2ed28fcf
Vitor Pamplona <vitor@vitorpamplona.com>
2022-03-01 17:51:18 -0500

    Generalizing the QRFinder class to be used in test cases.

--- app/src/main/java/org/who/ddccverifier/services/QRFinder.kt
+++ app/src/main/java/org/who/ddccverifier/services/QRFinder.kt
@@ -4,16 +4,22 @@ import android.annotation.SuppressLint
 import android.util.Base64
 import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.ImageProxy
+import com.google.android.gms.tasks.Tasks
 import com.google.mlkit.vision.barcode.BarcodeScanning
 import com.google.mlkit.vision.barcode.common.Barcode
 import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.barcode.BarcodeScannerOptions
+
+
+
 
 /**
  * Turns camera preview frames into QR Codes when found.
  */
-class QRFinder(private val onQRCodeFound: (Set<String>)->Unit): ImageAnalysis.Analyzer {
+class QRFinder(private val onQRCodeFound: (Set<String>)->Unit = { /* nop */ } ): ImageAnalysis.Analyzer {
 
     private var onlyTheFirstFrame: Boolean = false
+    private val scanningOptions = BarcodeScannerOptions.Builder().setBarcodeFormats(Barcode.FORMAT_QR_CODE).build()
 
     override fun analyze(imageProxy: ImageProxy) {
         scanBarcode(imageProxy)
@@ -23,17 +29,44 @@ class QRFinder(private val onQRCodeFound: (Set<String>)->Unit): ImageAnalysis.An
     private fun scanBarcode(imageProxy: ImageProxy) {
         imageProxy.image?.let { image ->
             val inputImage = InputImage.fromMediaImage(image, imageProxy.imageInfo.rotationDegrees)
-            val scanner = BarcodeScanning.getClient()
-            scanner.process(inputImage)
-                .addOnCompleteListener {
-                    imageProxy.close()
-                    if (it.isSuccessful) {
-                        readBarcodeData(it.result as List<Barcode>)
-                    } else {
-                        it.exception?.printStackTrace()
-                    }
-                }
+            scanBarcodes(inputImage, {
+                imageProxy.close()
+                finishOnFirstFrame(it)
+            }, {
+                imageProxy.close()
+            })
         }
+    }
+
+    fun scanBarcodes(
+        inputImage: InputImage,
+        onQRsFound: (Set<String>) -> Unit = { /* nop */ },
+        onQRsNotFound: () -> Unit = { /* nop */ }) {
+
+        BarcodeScanning.getClient(scanningOptions).process(inputImage)
+            .addOnSuccessListener { barcodes ->
+                if (barcodes.isNotEmpty()) {
+                    onQRsFound(barToStr(barcodes))
+                } else {
+                    onQRsNotFound()
+                }
+            }
+            .addOnCanceledListener(onQRsNotFound)
+            .addOnFailureListener {
+                it.printStackTrace()
+                onQRsNotFound()
+            }
+    }
+
+    fun scanBarcodesSync(inputImage: InputImage): Set<String> {
+        val scanner = BarcodeScanning.getClient(scanningOptions)
+        val task = scanner.process(inputImage)
+        Tasks.await(task)
+        task.exception?.printStackTrace()
+        if (task.result.isNotEmpty()) {
+            return barToStr(task.result)
+        }
+        return emptySet()
     }
 
     /**
@@ -43,16 +76,17 @@ class QRFinder(private val onQRCodeFound: (Set<String>)->Unit): ImageAnalysis.An
         if (array == null) return ""
         return "B64:" + Base64.encodeToString(array, Base64.DEFAULT)
     }
+
+    private fun barToStr(barcodes: List<Barcode>): Set<String> {
+        return barcodes.groupBy { it.rawValue ?: bytesToStr(it.rawBytes) }.keys
+    }
  
-    private fun readBarcodeData(barcodes: List<Barcode>) {
-        if (barcodes.isEmpty()) return
+    private fun finishOnFirstFrame(barcodes: Set<String>) {
         synchronized (this) {
             if (onlyTheFirstFrame) return
             onlyTheFirstFrame = true
         }
 
-        val qrStrings = barcodes.groupBy { it.rawValue ?: bytesToStr(it.rawBytes) }.keys
-
-        onQRCodeFound(qrStrings)
+        onQRCodeFound(barcodes)
     }
 }
