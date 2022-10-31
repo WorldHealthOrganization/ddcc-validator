636bef69bf56a585901e23cf121a4ba893c0d3a8
Vitor Pamplona <vitor@vitorpamplona.com>
2022-10-31 16:21:41 -0400

    Moving CBOR processing to a JSON parse

--- verify/src/main/java/org/who/ddccverifier/verify/hcert/CBORTranslator.kt
+++ verify/src/main/java/org/who/ddccverifier/verify/hcert/CBORTranslator.kt
@@ -1,10 +1,10 @@
 package org.who.ddccverifier.verify.hcert
 
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.upokecenter.cbor.CBORObject
 import org.hl7.fhir.r4.model.Bundle
-import org.hl7.fhir.r4.model.Composition
-import org.who.ddccverifier.verify.hcert.dcc.CBORParser
 import org.who.ddccverifier.map.hcert.dcc.DCC2FHIR
+import org.who.ddccverifier.verify.hcert.dcc.logical.WHOLogicalModel
 
 /**
  * Finds the right translator for the CBOR content using HC1.
@@ -14,7 +14,12 @@ class CBORTranslator {
 
     fun toFhir(hcertPayload: CBORObject): Bundle {
         if (hcertPayload[EU_DCC_CODE] != null)
-            return DCC2FHIR().run(CBORParser().parseCWT(hcertPayload))
+            return DCC2FHIR().run(
+                jacksonObjectMapper().readValue(
+                    hcertPayload.ToJSONString(),
+                    CWT::class.java
+                )
+            )
 
         return WHO2FHIR().run(hcertPayload)
     }
--- verify/src/main/java/org/who/ddccverifier/verify/hcert/dcc/LogicalModel.kt
+++ verify/src/main/java/org/who/ddccverifier/verify/hcert/dcc/LogicalModel.kt
@@ -1,16 +1,40 @@
 package org.who.ddccverifier.verify.hcert.dcc.logical
 
+import ca.uhn.fhir.model.api.TemporalPrecisionEnum
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.core.JsonParser
+import com.fasterxml.jackson.databind.DeserializationContext
+import com.fasterxml.jackson.databind.JsonDeserializer
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize
 import org.hl7.fhir.r4.model.*
 import org.who.ddccverifier.utils.FHIRLogical
+import org.who.ddccverifier.verify.shc.DecimalToDataTimeDeserializer
+import java.util.*
 
 class CWT (
+    @JsonProperty("1")
     val iss: StringType?,   // Issuer
+    @JsonProperty("2")
     val sub: StringType?,   // Subject
+    @JsonProperty("3")
     val aud: StringType?,   // Audience
+    @JsonProperty("4")
+    @JsonDeserialize(using = DecimalToDataTimeDeserializer::class)
     val exp: DateTimeType?, // expiration
+    @JsonProperty("5")
+    @JsonDeserialize(using = DecimalToDataTimeDeserializer::class)
     val nbf: DateTimeType?, // not before date
+    @JsonProperty("6")
+    @JsonDeserialize(using = DecimalToDataTimeDeserializer::class)
     val iat: DateTimeType?, // issued at date
+    @JsonProperty("7")
     val id: StringType?,   // Audience
+    @JsonProperty("-260")
+    val data: HCERT?,      // Certificate
+): FHIRLogical()
+
+class HCERT(
+    @JsonProperty("1")
     val cert: HC1?          // Cert
 ): FHIRLogical()
 
@@ -65,3 +89,15 @@ class Recovery(
     val `is`: StringType?,  // Certificate Issuer
     val ci:	StringType?     // Unique Certificate Identifier: UVCI
 ): FHIRLogical()
+
+
+object DecimalToDataTimeDeserializer: JsonDeserializer<DateTimeType>() {
+    private fun parseDateType(date: Double?): DateTimeType? {
+        if (date == null) return null
+        return DateTimeType(Date((date*1000).toLong()), TemporalPrecisionEnum.DAY)
+    }
+
+    override fun deserialize(p: JsonParser, ctxt: DeserializationContext): DateTimeType? {
+        return parseDateType(p.decimalValue?.toDouble())
+    }
+}
