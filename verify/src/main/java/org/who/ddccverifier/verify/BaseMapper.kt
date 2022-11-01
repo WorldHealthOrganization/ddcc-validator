d477da9fdbd3e175507727c9d3cf4a88f2a0f930
Vitor Pamplona <vitor@vitorpamplona.com>
2022-11-01 17:12:12 -0400

    Making sure resources are loaded by encoding bundle to string and parsing it again.

--- verify/src/main/java/org/who/ddccverifier/verify/BaseMapper.kt
+++ verify/src/main/java/org/who/ddccverifier/verify/BaseMapper.kt
@@ -1,5 +1,7 @@
 package org.who.ddccverifier.verify
 
+import ca.uhn.fhir.context.FhirContext
+import ca.uhn.fhir.context.FhirVersionEnum
 import org.hl7.fhir.r4.context.SimpleWorkerContext
 import org.hl7.fhir.r4.model.Base
 import org.hl7.fhir.r4.model.Bundle
@@ -15,9 +17,15 @@ open class BaseMapper {
             ""
         )
 
-        return Bundle().apply {
+        val bundle = Bundle().apply {
             StructureMapUtilities(simpleWorkerContext)
                 .transform(simpleWorkerContext, source, structureMap, this)
         }
+
+        // Is there a better way to load resources from references in a Bundle
+        val processor = FhirContext.forCached(FhirVersionEnum.R4).newJsonParser()
+        val strBundle = processor.encodeResourceToString(bundle)
+
+        return processor.parseResource(Bundle::class.java, strBundle)
     }
 }
